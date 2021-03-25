86c3b3e7f2aed99ed59b344cf9e7d8369533db66
cristinaj1 <cristinacee305@gmail.com>
2021-03-25 21:18:43 +0100

    Todo hecho menos el hacer split con el igual para que no de el error

--- src/main/java/calculadora/PanelPrincipal.java
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -50,46 +50,152 @@ public class PanelPrincipal extends JPanel implements ActionListener {
 
     }
 
+    public boolean controlExcepcion(String boton, JTextArea areaTexto) {
+        try {
+            Integer.parseInt(areaTexto.getText());
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
     @Override
     public void actionPerformed(ActionEvent ae) {
         // Se obtiene el objeto que desencadena el evento
         Object o = ae.getSource();
-        int resultado = 0;
+
+        String numeroA = "";
+        String numeroB = "";
+        Boolean intercambiar = true;
+        String operador = "";
+        int resultado;
+        String resultadoString;
+        StringBuilder sb = new StringBuilder();
+        String[] numeros = new String[2];
+        String operacion="";
         // Si es un botón
         if (o instanceof JButton) {
-            System.out.println(((JButton) o).getText());
-            areaTexto.setText(((JButton) o).getText());
-            areaTexto.setText(((JButton) o).getText());
+            //Está en este orden porque si no da error.
+            JButton prueba = (JButton) o;
+            System.out.println(prueba.getText());
+            //Para que nos muestre los numeros por pantalla controlando excepciones
+            if (!controlExcepcion(prueba.getText(), areaTexto)) {
+                areaTexto.setText(areaTexto.getText() + prueba.getText());
+            } else {
+                areaTexto.setText(areaTexto.getText() + prueba.getText());
+            }
 
-            //Esto es en lo que me tienes que ayudar
-            /* if (o.equals(botonera.grupoBotones[10])) {*/
-            //Lo veo como posible pero no lo veo
-//                for(int i = 0 < botonera.grupoBotones.length < i++){
-//                    
-//                }
-            for (JButton boton : this.botonera.getgrupoBotones()) {
-                if (boton.equals("+")) {
-                    resultado -= Integer.parseInt(areaTexto.getText());
-                    botonera.setEnabled(false);
+            if (prueba.getText().equals("+") || prueba.getText().equals("-") || prueba.getText().equals("*") || prueba.getText().equals("/")) {
+                for (int i = 10; i < this.botonera.getgrupoBotones().length - 1; i++) {
+                    //Para que se deshabilite los botones cuando le da a los operadores
+                    this.botonera.getgrupoBotones()[i].setEnabled(false);
                 }
             }
+            for (int i = 0; i < areaTexto.getText().length() - 1; i++) {
+                switch (areaTexto.getText().charAt(i)) {
+                            case '+':
+                                operador = "+";
+                                break;
+                            case '-':
+                                operador = "-";
+                                break;
+                            case '*':
+                                operador = "*";
+                                break;
+                            case '/':
+                                operador = "/";
+                                break;
 
-//                resultado += Integer.parseInt(areaTexto.getText());
-//                areaTexto.setEnabled(false);
-            /*} else*/ if (o.equals("-")) {
-
-            } else if (o.equals("*")) {
-                resultado *= Integer.parseInt(areaTexto.getText());
-                botonera.setEnabled(false);
-            } else if (o.equals("/")) {
-                resultado /= Integer.parseInt(areaTexto.getText());
-                botonera.setEnabled(false);
-            } else if (o.equals("=")) {
-                areaTexto.setText(" " + resultado);
-                botonera.setEnabled(false);
-                o = "";
+                        }
             }
-        }
+            for (int i = 0; i < areaTexto.getText().length() - 1; i++) {
+                sb.append(areaTexto.getText().charAt(i));
+            }
+            operacion = sb.toString();
+            System.out.println(operacion);
+            System.out.println(operador);
+            int numeroAInt = 1/*Integer.parseInt(numeroA)*/;
+            int numeroBInt = 2;
+            //Vuelve a activar los botones después de deshabilitarlos
+            for (int i = 0; i < 10; i++) {
+                if (prueba.getText().equals(Integer.toString(i)) || prueba.getText().equals("C")) {
+                    for (int j = 10; j < this.botonera.getgrupoBotones().length - 1; j++) {
+                        //Para que en el resto de botones sí puedan seguir siendo ejecutable
+                        //Vale también para hace numeros de mas de 1 dígito
+                        this.botonera.getgrupoBotones()[j].setEnabled(true);
+                    }
+                }
+            }
+            if (prueba.getText() == "C") {
+                areaTexto.setText("");
+            }
 
+            if (prueba.getText() == "=") {
+                System.out.println(areaTexto.getText());
+                switch (operador) {
+                    case "+":
+                        numeros = areaTexto.getText().split(operador);
+                        resultado = numeroAInt + numeroBInt;
+                        resultadoString = String.valueOf(resultado);
+                        areaTexto.setText(resultadoString);
+                        break;
+                    case "-":
+                        numeros = areaTexto.getText().split(operador);
+                        resultado = numeroAInt - numeroBInt;
+                        resultadoString = String.valueOf(resultado);
+                        areaTexto.setText(resultadoString);
+                        break;
+                    case "*":
+                        numeros = areaTexto.getText().split(operador);
+                        resultado = numeroAInt * numeroBInt;
+                        resultadoString = String.valueOf(resultado);
+                        areaTexto.setText(resultadoString);
+                        break;
+                    case "/":
+                        numeros = areaTexto.getText().split(operador);
+                        resultado = numeroAInt / numeroBInt;
+                        resultadoString = String.valueOf(resultado);
+                        areaTexto.setText(resultadoString);
+                        break;
+                        
+                }
+
+            }
+
+            switch (prueba.getText()) {
+                case "0":
+                    areaTexto.setText(areaTexto.getText());
+
+                    break;
+                case "1":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "2":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "3":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "4":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "5":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "6":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "7":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "8":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+                case "9":
+                    areaTexto.setText(areaTexto.getText());
+                    break;
+            }
+
+        }
     }
 }
